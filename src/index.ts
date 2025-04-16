6609c966ff031157f3a7826aaf5355e8c8605975
devucb <91523007+devucb@users.noreply.github.com>
2025-04-16 17:12:28 +0300

    feat: square

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export const sum = (a: number, b: number) => a + b;
+
+export const square = (a: number) => a * a;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,10 @@
-import { sum } from "../src";
+import { square, sum } from '../src';
 
-test("add 1 + 2 to equal 3", () => {
+test('add 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('square of 2', () => {
+  expect(square(2)).toBe(4);
+  expect(square(-1)).toBe(1);
 });
